a59d7cf3363bb1509bb41c8efe4b42d5d310011b
Tarek Elkheir <elkheirt@wit.edu>
2022-06-21 15:24:36 -0400

    Small updates to layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import {Card, CardContent, Container, Grid, Paper, Stack, Typography} from "@mui/material";
 import Chart from "./Chart";
+import InfoCard from "./InfoCard";
 
 function App() {
     return (<div className="App">
@@ -16,51 +17,26 @@ function App() {
                         <Grid item xs={8}>
                             <Chart></Chart>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Stack direction={"column"} alignItems={"center"} spacing={2} height={"60%"}>
-                                <Card variant={"outlined"} style={{height: "100%", width:"60%", textAlign: "center"}}>
-                                    <CardContent>
-                                        <Typography>
-                                            Current Temperature
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                                <Card variant={"outlined"} style={{height: "100%", width:"60%", textAlign: "center"}}>
-                                    <CardContent>
-                                        <Typography>
-                                            Current Humidity
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Stack>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Container>
-        </div>);
-}
-
-/*
-*                     <Stack direction={"row"}>
-                        <Chart></Chart>
                         <Stack direction={"column"} justifyContent={"center"} spacing={2}>
-                            <Card variant={"outlined"} style={{height: "40%", textAlign: "center"}}>
+                            <Card variant={"outlined"} style={{height: "30%", textAlign: "center"}}>
                                 <CardContent>
-                                    <Typography>
+                                    <Typography variant={"h5"}>
                                         Current Temperature
                                     </Typography>
                                 </CardContent>
                             </Card>
-                            <Card variant={"outlined"} style={{height: "40%", textAlign: "center"}}>
+                            <Card variant={"outlined"} style={{height: "30%", textAlign: "center"}}>
                                 <CardContent>
-                                    <Typography>
+                                    <Typography variant={"h5"}>
                                         Current Humidity
                                     </Typography>
                                 </CardContent>
                             </Card>
                         </Stack>
-                    </Stack>
-*
-* */
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>);
+}
 
 export default App;
