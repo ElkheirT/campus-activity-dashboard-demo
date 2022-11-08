315b8680643cd7317c7ae591414c67b30aee7d05
ElkheirT <elkheirt@wit.edu>
2022-11-08 09:33:27 -0500

    Comment out socketio code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import temperatureIcon from './images/thermometer.svg'
 import humidityIcon from './images/humidity.svg'
-import RefreshIcon from '@mui/icons-material/Refresh';
 import {
     AppBar,
     Box, Button,
@@ -12,8 +11,6 @@ import {
     Typography
 } from "@mui/material";
 import Chart from "./Chart";
-import io from 'socket.io-client'
-import {db} from './db'
 import {useEffect, useState} from "react";
 import InfoCard from "./InfoCard";
 import Histogram from "./Histogram";
@@ -33,14 +30,14 @@ function App() {
     prevOpenTime.setDate(prevOpenTime.getDate() - 1);
     prevCloseTime.setDate(prevCloseTime.getDate() - 1);
 
-    const socket = io('localhost:5000');
+    // const socket = io('localhost:5000');
 
     const [histogramDate, setHistogramDate] = useState(prevOpenTime);
     const [histogramData, setHistogramData] = useState([]);
 
     const [temp, setTemp] = useState(70);
     const [humidity, setHumidity] = useState(67);
-    
+
     const getRandomInt = (min, max) => {
         return Math.floor(Math.random() * (max - min) + min);
     }
@@ -62,84 +59,84 @@ function App() {
         setHistogramData(newData);
     };
 
-    const handleHistogramDataChange = (newData) => {
-        let dataAsJSON = JSON.parse(newData);
-        let dataAsArray = [];
-        let keys = Object.keys(dataAsJSON);
-        for (const key of keys) {
-            let entry = dataAsJSON[key];
-            dataAsArray.push({x: key, y: entry});
-        }
-        setHistogramData(dataAsArray);
-    }
+    // const handleHistogramDataChange = (newData) => {
+    //     let dataAsJSON = JSON.parse(newData);
+    //     let dataAsArray = [];
+    //     let keys = Object.keys(dataAsJSON);
+    //     for (const key of keys) {
+    //         let entry = dataAsJSON[key];
+    //         dataAsArray.push({x: key, y: entry});
+    //     }
+    //     setHistogramData(dataAsArray);
+    // }
 
-    useEffect(() => {
-        if (!localStorage.getItem('lastFetchTime')) {
-            localStorage.setItem('lastFetchTime', openTime.toString());
-        }
+    // useEffect(() => {
+    //     if (!localStorage.getItem('lastFetchTime')) {
+    //         localStorage.setItem('lastFetchTime', openTime.toString());
+    //     }
+    //
+    //     getLatestData();
+    //
+    //     let queryString = `${prevOpenTime.getFullYear()}-${prevOpenTime.getMonth() + 1}-${prevOpenTime.getDate()}`;
+    //     socket.emit('get_binned_data', queryString);
+    //
+    //     socket.emit('get_data_stream');
+    //
+    //     socket.on('binned_data', (data) => {
+    //         handleHistogramDataChange(data);
+    //     });
+    //
+    //     socket.on('past_data', (data) => {
+    //         console.log('got past data', data);
+    //         if (data.length > 0) {
+    //             data.forEach((element) => {
+    //                 addToDB(element);
+    //             });
+    //             let mostRecentFetch = data[data.length - 1].time_stamp;
+    //             localStorage.setItem('lastFetchTime', mostRecentFetch);
+    //         }
+    //     });
+    //
+    //     socket.on('new_data_point', (data) => {
+    //         if (data.sensor_type === "motion") {
+    //             addToDB(data);
+    //         } else if (data.sensor_type === "temp") {
+    //             setTemp(data.sensor_output);
+    //         } else if (data.sensor_type === "humidity") {
+    //             setHumidity(data.sensor_output);
+    //         }
+    //         let mostRecentFetch = data.time_stamp;
+    //         localStorage.setItem('lastFetchTime', mostRecentFetch);
+    //     });
+    // }, []);
 
-        getLatestData();
+    // function getLatestData() {
+    //     let lastFetchTime = localStorage.getItem('lastFetchTime');
+    //     getDataInRange(new Date(lastFetchTime), closeTime);
+    // }
 
-        let queryString = `${prevOpenTime.getFullYear()}-${prevOpenTime.getMonth() + 1}-${prevOpenTime.getDate()}`;
-        socket.emit('get_binned_data', queryString);
+    // function getDataInRange(startDate, endDate) {
+    //     socket.emit('get_data_in_range', {
+    //         startDate: dateToString(startDate),
+    //         endDate: dateToString(endDate)
+    //     });
+    // }
 
-        socket.emit('get_data_stream');
+    // function dateToString(date) {
+    //     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+    // }
 
-        socket.on('binned_data', (data) => {
-            handleHistogramDataChange(data);
-        });
-
-        socket.on('past_data', (data) => {
-            console.log('got past data', data);
-            if (data.length > 0) {
-                data.forEach((element) => {
-                    addToDB(element);
-                });
-                let mostRecentFetch = data[data.length - 1].time_stamp;
-                localStorage.setItem('lastFetchTime', mostRecentFetch);
-            }
-        });
-
-        socket.on('new_data_point', (data) => {
-            if (data.sensor_type === "motion") {
-                addToDB(data);
-            } else if (data.sensor_type === "temp") {
-                setTemp(data.sensor_output);
-            } else if (data.sensor_type === "humidity") {
-                setHumidity(data.sensor_output);
-            }
-            let mostRecentFetch = data.time_stamp;
-            localStorage.setItem('lastFetchTime', mostRecentFetch);
-        });
-    }, []);
-
-    function getLatestData() {
-        let lastFetchTime = localStorage.getItem('lastFetchTime');
-        getDataInRange(new Date(lastFetchTime), closeTime);
-    }
-
-    function getDataInRange(startDate, endDate) {
-        socket.emit('get_data_in_range', {
-            startDate: dateToString(startDate),
-            endDate: dateToString(endDate)
-        });
-    }
-
-    function dateToString(date) {
-        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
-    }
-
-    async function addToDB(data) {
-        try {
-            /* store data.time_stamp as a JS Date object,
-            so we can run date queries using dexie.js */
-            let timeStamp = data.time_stamp;
-            data.time_stamp = new Date(timeStamp);
-            const id = await db.sensorData.add(data);
-        } catch (error) {
-            console.log("Failed to add data: ", error);
-        }
-    }
+    // async function addToDB(data) {
+    //     try {
+    //         /* store data.time_stamp as a JS Date object,
+    //         so we can run date queries using dexie.js */
+    //         let timeStamp = data.time_stamp;
+    //         data.time_stamp = new Date(timeStamp);
+    //         const id = await db.sensorData.add(data);
+    //     } catch (error) {
+    //         console.log("Failed to add data: ", error);
+    //     }
+    // }
 
     return (
         <LocalizationProvider dateAdapter={AdapterLuxon}>
@@ -165,7 +162,7 @@ function App() {
                                 text={humidity}
                             />
                         </Stack>
-                        <Grid container justifyContent={"center"} spacing={5}>
+                        <Grid container justifyContent={"center"} alignItems={"flex-start"} spacing={5}>
                             <Grid item lg={6} md={8} sm={10}>
                                 <Chart openTime={openTime} data={data}/>
                             </Grid>
